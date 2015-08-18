de65f0cb6dac2cfef2b3e1d13e98f719c42a5a26
Saman <saman.dadmand@gmail.com>
2015-08-18 12:10:27 +0430

    Version 4.0.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 import py2exe
 
 setup(name='WhoisScanner',
-      version='3.7',
+      version='4.0.0',
       description='Scan Whois',
       author='Saman Dadmand',
       author_email='saman.dadmand@gmail.com',
-      console=["ListMain.py"],
+      console=["Main.py"],
       requires=['tabulate', 'mysql', 'cryptography', 'colorama', 'colorama']
       )
