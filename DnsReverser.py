fe00d6a8c936a0a2b9729b2dbd937abae2a4c8a2
Saman <saman.dadmand@gmail.com>
2015-08-18 12:00:04 +0430

    Version 4.0.0

--- DnsReverser.py
+++ DnsReverser.py
@@ -2,7 +2,7 @@ __author__ = 'Saman'
 from mysql.connector import MySQLConnection
 from config import read_db_config
 from config import config_section_map
-from colorama import Fore, Back, Style
+from colorama import Fore
 from colorama import init
 import datetime
 from time import sleep
@@ -20,7 +20,7 @@ def db_select(query):
             query_result_exec.execute(query)
             query_result = query_result_exec.fetchall()
             db_reconnect_status += 1
-            return query_result
+            return query_result, query_result_exec.rowcount
         except Exception:
             db_reconnect2_status = 0
             while 0 == db_reconnect2_status:
@@ -78,34 +78,28 @@ def main():
 
     if config_section_map("dns_reverser")['default_list_name']:
         selected_list = str(config_section_map("dns_reverser")['default_list_name'])
-        loop_counter = 1
+        todo_result_rows = 1
         process_counter = 0
-        while loop_counter > 0:
-            # lock record for process
-            lock_record_query = 'update %s set lockedby=connection_id(),lockedat=now(),HFlag=\'D\' ' \
-                                'where lockedby is null and status=\'Site registered\' and HFlag=\'R\' ' \
-                                'and ip!=\'Fail\' and ip is not null limit %s' \
-                                % (selected_list, int(config_section_map("dns_reverser")['buffer']))
-            loop_counter, connection_d = db_update(lock_record_query)
+        while todo_result_rows > 0:
+            todo_query = 'select distinct(ip) from %s where ip is not null and ip!=\'Fail\' and ip not in ' \
+                         '(select refrence from harverster_hosts) limit %s' % \
+                         (selected_list, int(config_section_map("dns_reverser")['buffer']))
+            todo_result, todo_result_rows = db_select(todo_query)
 
-            if loop_counter:
-                todo_query = 'select distinct(ip) from %s where  lockedby=%s ' % (
-                    selected_list, connection_d)
-                todo_result = db_select(todo_query)
-                if todo_result:
+            if todo_result:
 
-                    for to_do_line in range(0, len(todo_result)):
-                        process_counter += 1
+                for to_do_line in range(0, len(todo_result)):
+                    process_counter += 1
 
-                        look_for = todo_result[to_do_line][0]
-                        looking_for = (str(look_for.strip()))
+                    look_for = todo_result[to_do_line][0]
+                    looking_for = (str(look_for.strip()))
 
-                        reverse_result = reverse_dns(looking_for)
-                        for line in reverse_result:
-                            return_insert_query = "insert into  harverster_hosts (host,refrence) " \
-                                                  "values ('%s','%s')" \
-                                                  % ((reverse_result[line][0])[:100], todo_result[to_do_line][0])
-                            db_update(return_insert_query)
+                    reverse_result = reverse_dns(looking_for)
+                    for line in reverse_result:
+                        return_insert_query = "insert into  harverster_hosts (host,refrence) " \
+                                              "values ('%s','%s')" \
+                                              % ((reverse_result[line][0])[:100], todo_result[to_do_line][0])
+                        db_update(return_insert_query)
 
 
 if __name__ == '__main__':
